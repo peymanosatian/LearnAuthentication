77b46597038a1d6c090e3a9267e037363ac82d7e
Peyman <peyman.osatian@gmail.com>
2024-08-17 12:11:55 +0200

    SignInComplited

---
+++ LearnAuthentication/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  LearnAuthentication
+//
+//  Created by Peyman Osatian on 2024-08-17.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
