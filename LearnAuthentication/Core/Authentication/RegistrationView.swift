354893d6d62e773910c0ee2f0ee2af7ec24efdbc
Peyman <peyman.osatian@gmail.com>
2024-08-18 12:51:49 +0200

    SIGNUP AND PROFILE UI

---
+++ LearnAuthentication/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  LearnAuthentication
+//
+//  Created by Peyman Osatian on 2024-08-18.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ LearnAuthentication/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  LearnAuthentication
+//
+//  Created by Peyman Osatian on 2024-08-18.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
